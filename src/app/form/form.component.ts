0b5183933ba98f5fad754c068d51edede0decead
Dhyder <victor.wairiguini@student.moringaschool.com>
2021-10-12 01:50:22 +0300

    formtypescript

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from '@angular/compiler';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quoter } from '../quoter';
 
 @Component({
   selector: 'app-form',
@@ -6,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+  @Output() popQuote= new EventEmitter()
+  yourName!: string;
+  quoteTitle!: string;
+  quote!: string;
+  quoteAuthor!: string;
+  poppedQuote:any
+
+  publishQuote(){
+    this.poppedQuote = new Quoter(this.quoteTitle, this.yourName, this.quoteAuthor, this.quote)
+    this.quoteTitle= ''
+    this.yourName= ''
+    this.quoteAuthor = ''
+    this.quote= ''
+    this.popQuote.emit(this.poppedQuote)
+  }
 
   constructor() { }
 
